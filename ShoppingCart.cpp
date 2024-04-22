d7e2aa6dd6efed26b298be021615f23a3be72984
jacksonthomas1 <167336438+jacksonthomas1@users.noreply.github.com>
2024-04-22 13:16:32 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -23,7 +23,45 @@ string ShoppingCart::GetCustomerDate() const {
 } 
 
 void ShoppingCart::AddItem(ItemToPurchase item) { 
+    cartItems.push_back(item);
 }
+
+// still needs work
+void ShoppingCart::RemoveItem(string name) { 
+  for (int i = 0; i < cartItems.length(); i++) { 
+      if (cartItems.at(i).GetName() == name) { 
+        cartItems.at(i).erase();
+        break;
+      }
+  }
+} 
+
+//also needs work 
+void ShoppingCart::ModifyItem(ItemToPurchase item) { 
+} 
+
+int ShoppingCart::GetNumItemsInCart() { 
+  int quantityOfItems = 0;
+  for (int i = 0; i < cartItems.length(); i++) { 
+    quantityOfItems = quantityOfItems + cartItems.at(i).GetQuantity();
+  }
+  return quantityOfItems; 
+} 
+
+double ShoppingCart::GetCostOfCart() { 
+  int totalCost;
+  for (int i = 0; i < cartItems.length(); i++) { 
+    totalCost = totalCost + (cartItems.at(i).GetPrice() * cartItems.at(i).getQuantity());
+  }
+  return totalCost; 
+} 
+
+
+
+
+
+
+
 
 
 
