317a2ef7a7e85a08680d3a3b3922dd69c8bedd94
jacksonthomas1 <167336438+jacksonthomas1@users.noreply.github.com>
2024-04-22 11:25:13 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+#include "ItemToPurchase.h"
+
+ItemToPurchase::ItemToPurchase(string newItemName, int newItemPrice, int newItemQuantity) { 
+   itemName = newItemName; 
+   itemPrice = newItemPrice; 
+   itemQuantity = newItemQuantity; 
+} 
+void ItemToPurchase::SetName(string newName) { 
+   itemName = newName;
+}
+string ItemToPurchase::GetName() { 
+   return itemName; 
+} 
+void ItemToPurchase::SetPrice(int newPrice) { 
+   itemPrice = newPrice; 
+} 
+int ItemToPurchase::GetPrice() { 
+   return itemPrice; 
+} 
+void ItemToPurchase::SetQuantity(int newQuantity) { 
+   itemQuantity = newQuantity; 
+} 
+int ItemToPurchase::GetQuantity() { 
+   return itemQuantity; 
+} 
