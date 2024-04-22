82e54393bc4a75db54c5153ea87ecfac457ef2f2
jacksonthomas1 <167336438+jacksonthomas1@users.noreply.github.com>
2024-04-22 11:49:33 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,32 @@
+#include<iostream> 
+#include<string>
+#include<vector>
+using namespace std; 
+
+#include "ShoppingCart.h"
+
+ShoppingCart::ShoppingCart() { 
+  customerName = ""; 
+  customerDate = ""; 
+} 
+ShoppingCart::ShoppingCart(string name, string date) { 
+  customerName = name; 
+  customerDate = date; 
+}
+
+string ShoppingCart::GetCustomerName() const  {
+  return customerName; 
+}
+
+string ShoppingCart::GetCustomerDate() const { 
+  return customerDate; 
+} 
+
+void ShoppingCart::AddItem(ItemToPurchase item) { 
+}
+
+
+
+
+
 
