5f1491cf9b1671e7259e2690530c81d0c498dad3
jacksonthomas1 <167336438+jacksonthomas1@users.noreply.github.com>
2024-04-24 11:16:26 -0500

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -5,17 +5,56 @@ using namespace std;
 #include "ShoppingCart.h"
 
 void PrintMenu() {
-   /* Type your code here */
-   
+   cout << "MENU" << endl; 
+   cout << "a - Add item to cart" << endl; 
+   cout << "d - Remove item from cart" << endl; 
+   cout << "c - Change item quantity" << endl; 
+   cout << "i - Output items' description" << endl; 
+   cout << "o - Output shopping cart" << endl;
+   cout << "q - Quit" << endl;
 }
 
+//needs work 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
+
+   if (option == 'a') { 
+      theCart.AddItem(); 
+   } 
+   if (option == 'd') { 
+      theCart.RemoveItem();
+   } 
+   //needs work
+   if (option == 'c') { 
+      theCart.ModifyItem(); 
+   } 
+   if (option == 'i') { 
+      theCart.PrintDescriptions();
+   } 
+   if (option == 'o') { 
+      theCart.PrintTotal();
+   } 
+   //needs work
+   if (option == 'q') { 
+   }
+      
    
 }
 
 int main() {
-   /* Type your code here */
+
+   ShoppingCart cartItems;
+
+   string customersName; 
+   string customerDate; 
+
+   cout << "Enter customers's name: " << endl; 
+   cin >> customersName; 
+
+   cout << "Enter today's date: " << endl;
+   cin  >> customerDate;
+
+   cout << "Customer name: " << customerName << endl; 
+   cout << "Today's date: " << customerDate << endl;
    
    return 0;
 }
